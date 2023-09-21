cdcaba31de426d988d39fed4c5c1e52f25bddddd
Findus06 <vilirask06@gmail.com>
2023-09-21 12:06:32 +0300

    Added Advanced Ping command

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,12 +1,18 @@
 module.exports = {
     name: 'ping',
-    description: 'Pong!',
+    description: 'Replies with bot ping!',
     // devOnly: Boolean,
     // testOnly: Boolean,
     // option: Object[],
     // deleted: Boolean
 
-    callback: (client, interaction) => {
-        interaction.reply(`Pong! ${client.ws.ping}ms`);
+    callback: async (client, interaction) => {
+        await interaction.deferReply();
+
+        const reply = await interaction.fetchReply();
+
+        const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+        interaction.editReply(`Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`);
     },
 };
